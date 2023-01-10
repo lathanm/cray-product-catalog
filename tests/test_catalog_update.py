63e333026842ff948b9bebe65778bab0edf64801
Eli Kamin <eli.kamin@hpe.com>
2023-01-10 16:39:40 -0600

    Continue to fix up unit tests
    This commit continues to fix up the unit tests for catalog_update.py,
    including adding a unit test that reproduces the infinite hang fixed
    with CASM-3589.

--- tests/test_catalog_update.py
+++ tests/test_catalog_update.py
@@ -1,6 +1,6 @@
 # MIT License
 #
-# (C) Copyright 2022 Hewlett Packard Enterprise Development LP
+# (C) Copyright 2022-2023 Hewlett Packard Enterprise Development LP
 #
 # Permission is hereby granted, free of charge, to any person obtaining a
 # copy of this software and associated documentation files (the "Software"),
@@ -22,7 +22,7 @@
 #
 # Unit tests for catalog_update
 
-import copy
+from copy import deepcopy
 import unittest
 from unittest.mock import patch, Mock
 
@@ -36,14 +36,15 @@ from cray_product_catalog.catalog_update import (
 )
 
 
-class TestCatalogUpdate(unittest.TestCase):
+class TestUpdateConfigMap(unittest.TestCase):
+    """Test cases for the update_config_map function."""
+
     def setUp(self):
         """Set up patches."""
         self.mock_api_client = patch('cray_product_catalog.catalog_update.ApiClient').start()
         self.mock_core_v1_api = patch('cray_product_catalog.catalog_update.CoreV1Api').start()
         self.mock_core_v1_api_object = self.mock_core_v1_api.return_value
-        self.mock_product_catalog_data = copy.deepcopy(MOCK_PRODUCT_CATALOG_DATA)
-        self.original_product_catalog_data = copy.deepcopy(self.mock_product_catalog_data)
+        self.mock_product_catalog_data = deepcopy(MOCK_PRODUCT_CATALOG_DATA)
         self.mock_product_catalog_resource_version = '123'
         patch('cray_product_catalog.catalog_update.V1ConfigMap', Mock).start()
         self.mock_config_map_response = self.mock_core_v1_api_object.read_namespaced_config_map.return_value
@@ -52,7 +53,6 @@ class TestCatalogUpdate(unittest.TestCase):
         self.mock_patch_config_map = patch(
             'cray_product_catalog.catalog_update.patch_config_map', side_effect=self.fake_patch_config_map
         ).start()
-
         fake_env = {
             'PRODUCT': 'example-product',
             'PRODUCT_VERSION': '1.0.0'
@@ -70,7 +70,20 @@ class TestCatalogUpdate(unittest.TestCase):
         """Fake version of patch_config_map."""
         # NOTE: "patch" is slightly inaccurate here in that this function completely
         # overwrites the target data, but it is close enough for the purposes of this testing.
-        self.mock_core_v1_api_object.read_namespaced_config_map.return_value.data = config_map_data
+        self.mock_core_v1_api_object.read_namespaced_config_map.return_value.data = deepcopy(config_map_data)
+
+    def fake_get_config_map(self):
+        """Return the data returned by reading the config map."""
+        return self.mock_core_v1_api_object.read_namespaced_config_map.return_value.data
+
+    @staticmethod
+    def _set_product_version_active(product_version, active_version):
+        """Modify a dictionary of product versions to set one of them as 'active'."""
+        for version, version_data in product_version.items():
+            if version == active_version:
+                version_data['active'] = True
+            else:
+                version_data['active'] = False
 
     def tearDown(self):
         """Stop patches."""
@@ -79,13 +92,13 @@ class TestCatalogUpdate(unittest.TestCase):
     def test_update_config_map_empty(self):
         """Test calling update_config_map and inserting empty data for a new product."""
         update_config_map(data={}, namespace='mock_namespace', name='mock_config_map')
-        expected_product_catalog_data = self.original_product_catalog_data
-        expected_product_catalog_data['example-product'] = yaml.safe_dump({
+        expected_product_catalog_patch = self.mock_product_catalog_data
+        expected_product_catalog_patch['example-product'] = yaml.safe_dump({
             '1.0.0': {}
         })
         self.mock_patch_config_map.assert_called_once_with(
             self.mock_core_v1_api_object, 'mock_config_map', 'mock_namespace',
-            self.mock_product_catalog_resource_version, self.original_product_catalog_data
+            self.mock_product_catalog_resource_version, expected_product_catalog_patch
         )
 
     def test_update_config_map_with_data(self):
@@ -93,13 +106,14 @@ class TestCatalogUpdate(unittest.TestCase):
         update_config_map(
             data={'vcs': {'clone_url': 'https://example.com'}}, namespace='mock_namespace', name='mock_config_map'
         )
-        expected_product_catalog_data = self.original_product_catalog_data
-        expected_product_catalog_data['example-product'] = yaml.safe_dump({
+
+        expected_product_catalog_patch = self.mock_product_catalog_data
+        expected_product_catalog_patch['example-product'] = yaml.safe_dump({
             '1.0.0': {'vcs': {'clone_url': 'https://example.com'}}
         })
         self.mock_patch_config_map.assert_called_once_with(
             self.mock_core_v1_api_object, 'mock_config_map', 'mock_namespace',
-            self.mock_product_catalog_resource_version, self.original_product_catalog_data
+            self.mock_product_catalog_resource_version, expected_product_catalog_patch
         )
 
     def test_update_config_map_with_active(self):
@@ -110,22 +124,15 @@ class TestCatalogUpdate(unittest.TestCase):
             'SET_ACTIVE_VERSION': "true"
         })
         update_config_map(data={}, namespace='mock_namespace', name='mock_config_map')
-        expected_product_catalog_data = self.original_product_catalog_data
-        sat_data = yaml.safe_load(expected_product_catalog_data['sat'])
 
-        # Create expected 'sat' data with the desired version set active.
-        new_sat_data = {}
-        for version, version_data in sat_data.items():
-            if version == '2.0.0':
-                version_data['active'] = True
-            else:
-                version_data['active'] = False
-            new_sat_data[version] = version_data
-        expected_product_catalog_data['sat'] = yaml.safe_dump(new_sat_data)
+        expected_product_catalog_patch = self.mock_product_catalog_data
+        sat_patch_data = yaml.safe_load(expected_product_catalog_patch['sat'])
+        self._set_product_version_active(sat_patch_data, active_version='2.0.0')
+        expected_product_catalog_patch['sat'] = yaml.safe_dump(sat_patch_data)
 
         self.mock_patch_config_map.assert_called_once_with(
             self.mock_core_v1_api_object, 'mock_config_map', 'mock_namespace',
-            self.mock_product_catalog_resource_version, expected_product_catalog_data
+            self.mock_product_catalog_resource_version, expected_product_catalog_patch
         )
 
     def test_update_config_map_switch_active(self):
@@ -133,14 +140,8 @@ class TestCatalogUpdate(unittest.TestCase):
 
         # Set up the config map with '1.0.0' as 'active'
         original_mock_sat_data = yaml.safe_load(self.mock_product_catalog_data['sat'])
-        new_mock_sat_data = {}
-        for version, version_data in original_mock_sat_data.items():
-            if version == '1.0.0':
-                version_data['active'] = True
-            else:
-                version_data['active'] = False
-            new_mock_sat_data[version] = version_data
-        self.mock_product_catalog_data['sat'] = yaml.safe_dump(new_mock_sat_data)
+        self._set_product_version_active(original_mock_sat_data, '1.0.0')
+        self.mock_product_catalog_data['sat'] = yaml.safe_dump(original_mock_sat_data)
 
         self.set_up_mock_environment({
             'PRODUCT': 'sat',
@@ -150,37 +151,23 @@ class TestCatalogUpdate(unittest.TestCase):
 
         update_config_map(data={}, namespace='mock_namespace', name='mock_config_map')
 
-        expected_product_catalog_data = self.original_product_catalog_data
-        sat_data = yaml.safe_load(expected_product_catalog_data['sat'])
-
-        # Create expected 'sat' data with the desired version set active.
-        new_sat_data = {}
-        for version, version_data in sat_data.items():
-            if version == '2.0.0':
-                version_data['active'] = True
-            else:
-                version_data['active'] = False
-            new_sat_data[version] = version_data
-        expected_product_catalog_data['sat'] = yaml.safe_dump(new_sat_data)
+        expected_product_catalog_patch = self.mock_product_catalog_data
+        sat_data = yaml.safe_load(expected_product_catalog_patch['sat'])
+        self._set_product_version_active(sat_data, active_version='2.0.0')
+        expected_product_catalog_patch['sat'] = yaml.safe_dump(sat_data)
 
         self.mock_patch_config_map.assert_called_once_with(
             self.mock_core_v1_api_object, 'mock_config_map', 'mock_namespace',
-            self.mock_product_catalog_resource_version, expected_product_catalog_data
+            self.mock_product_catalog_resource_version, expected_product_catalog_patch
         )
 
     def test_update_config_map_clear_active(self):
         """Test clearing the 'active' field when one was active before."""
 
-        # Set up the config map with '1.0.0' as 'active'
+        # Set up the config map with '2.0.0' as 'active'
         original_mock_sat_data = yaml.safe_load(self.mock_product_catalog_data['sat'])
-        new_mock_sat_data = {}
-        for version, version_data in original_mock_sat_data.items():
-            if version == '1.0.0':
-                version_data['active'] = True
-            else:
-                version_data['active'] = False
-            new_mock_sat_data[version] = version_data
-        self.mock_product_catalog_data['sat'] = yaml.safe_dump(new_mock_sat_data)
+        self._set_product_version_active(original_mock_sat_data, active_version='2.0.0')
+        self.mock_product_catalog_data['sat'] = yaml.safe_dump(original_mock_sat_data)
 
         self.set_up_mock_environment({
             'PRODUCT': 'sat',
@@ -190,7 +177,19 @@ class TestCatalogUpdate(unittest.TestCase):
 
         update_config_map(data={}, namespace='mock_namespace', name='mock_config_map')
 
-        self.mock_patch_config_map.assert_called_once_with(
-            self.mock_core_v1_api_object, 'mock_config_map', 'mock_namespace',
-            self.mock_product_catalog_resource_version, self.original_product_catalog_data
-        )
+    @unittest.skip('This test only passes with the hotfix from CASM-3589')
+    def test_insert_repo_component_versions_already_present(self):
+        """Test inserting 'repositories' data into the component_versions when it is already present."""
+        data_to_insert = {
+            'component_versions': {
+                'repositories': [
+                    {'name': 'sat-sle-15sp2', 'type': 'group', 'members': ['sat-2.0.0-sle-15sp2']},
+                    {'name': 'sat-2.0.0-sle-15sp2', 'type': 'hosted'}
+                ]
+            }
+        }
+        self.set_up_mock_environment({
+            'PRODUCT': 'sat',
+            'PRODUCT_VERSION': '2.0.0'
+        })
+        update_config_map(data=data_to_insert, namespace='mock_namespace', name='mock_config_map')
